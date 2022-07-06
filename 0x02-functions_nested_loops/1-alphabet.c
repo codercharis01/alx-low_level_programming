956c8bc2946104381a1c33ec4f8d8894b6125d17
martyna charis martyns <ttttynaclassy@gmail.com>
2022-07-06 01:09:08 +0000

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- *print_alphabet ->prints the lowercase alphabets
- *
+ *print_alphabet -> prints the lowercase alphabets
  */
 void print_alphabet(void)
 {
 int j;
+
+id print_alphabet(void)
 for (j = 'a'; j<= 'z'; j++)
-{
-_putchar (j);
-}
+       {
+         _putchar (j);
+       }
 _putchar('\n');
 }
