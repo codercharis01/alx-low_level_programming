c5d9459a1e82c420d9970b8ebb0be811ea0be01e
martyna charis martyns <ttttynaclassy@gmail.com>
2022-07-06 00:14:11 +0000

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
  * main - prints putchar
  * Return: always 0
  */
@@ -13,6 +13,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');    
+_putchar('\n');  
 return (0);
 }
